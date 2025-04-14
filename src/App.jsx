ba96564fa52e77ef80cbda5aa2aab18eb65d5ccb
Tom Wetz <tomer2138@gmail.com>
2025-04-14 14:33:02 +0300

    Initial clean deploy

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-5xl font-bold">tomerw</h1>
+      <p className="mt-4 text-xl">Security Researcher & Engineer</p>
+    </div>
+  );
+}
